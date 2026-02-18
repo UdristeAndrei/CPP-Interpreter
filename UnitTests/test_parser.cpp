f8845ee9e9616d3a99c944c9194577876577a582
UdristeAndrei <andrei.udriste.98@gmail.com>
2026-02-18 15:56:26 +0100

    Finished testLetStatement

--- UnitTests/test_parser.cpp
+++ UnitTests/test_parser.cpp
@@ -1,7 +1,23 @@
-#include "parser.h"
+#include "../parser.h"
 
 bool testLetStatement(std::shared_ptr<Statement> statement, std::string& name) {
-    
+    if (statement->TokenLiteral() != "let") {
+        printf("statement->TokenLiteral is not 'let'. Got=%s", statement->TokenLiteral().c_str());
+        return false;
+    }
+
+    auto letStmt = std::dynamic_pointer_cast<LetStatement>(statement);
+    if (letStmt == nullptr) {
+        printf("statement is not LetStatement");
+        return false;
+    } else if (letStmt->Name->Value != name) {
+        printf("letStmt->Name->Value not '%s'. Got=%s", name.c_str(), letStmt->Name->Value.c_str());
+        return false;
+    } else if (letStmt->Name->TokenLiteral() != name) {
+        printf("statement->name not '%s'. Got=%s", name.c_str(), letStmt->Name->TokenLiteral().c_str());
+        return false;
+    }
+    return true;
 }
 
 void TestLetStatements() {
@@ -17,10 +33,12 @@ void TestLetStatements() {
 
     if (program == nullptr) {
         printf("ParseProgram() return nullptr");
+        return;
     }
 
     if (program->statements.size() != 3) {
-        printf("program.statements does not contain 3 statements. Got=%d", program->statements.size());
+        printf("program.statements does not contain 3 statements. Got=%li", program->statements.size());
+        return;
     }
 
     std::vector<std::string> tests = {"x", "y", "foobar"};
@@ -31,4 +49,8 @@ void TestLetStatements() {
             return;
         }
     }
+}
+
+int main() {
+    TestLetStatements();
 }
