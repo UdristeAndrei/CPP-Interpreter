86f5217efe1414b8a062add3cbbb05fbf063c38a
UdristeAndrei <udriste.andrei.98@gmail.com>
2026-02-19 21:11:37 +0100

    Added constructors to all the ast classes.

--- ast.h
+++ ast.h
@@ -4,18 +4,21 @@
 
 class Node {
     public:
+        Node() = default;
         virtual ~Node() = default;
         virtual std::string TokenLiteral() const = 0;
 };
 
 class Statement : public Node {
     public:
+        Statement() = default;
         virtual ~Statement() = default;
         virtual void statementNode() const = 0;
 };
 
 class Expression : public Node {
     public:
+        Expression() = default;
         virtual ~Expression() = default;
         virtual void expressionNode() const = 0;
 };
@@ -24,6 +27,10 @@ class Program : public Node {
     public:
         std::vector<std::shared_ptr<Statement>> statements{};
 
+        Program() = default;
+        Program(const std::vector<std::shared_ptr<Statement>>& stmts) : statements(stmts) {};
+        ~Program() = default;
+
         std::string TokenLiteral() const override {
             return statements.empty() ? "" : statements.front()->TokenLiteral();
         }
@@ -31,8 +38,12 @@ class Program : public Node {
 
 class Identifier : public Expression {
     public:
-        Token TokenIdent;
-        std::string Value;
+        Token TokenIdent{};
+        std::string Value{};
+
+        Identifier() = default;
+        Identifier(const Token& t, const std::string& s) : TokenIdent(t), Value(s) {};
+        ~Identifier() = default;
 
         void expressionNode() const override {};
         std::string TokenLiteral() const override { return TokenIdent.Literal; };
@@ -40,9 +51,14 @@ class Identifier : public Expression {
 
 class LetStatement : public Statement {
     public:
-        Token TokenStatement;
-        std::shared_ptr<Identifier> Name;
-        std::shared_ptr<Expression> Value;
+        Token TokenStatement{};
+        std::shared_ptr<Identifier> Name{};
+        std::shared_ptr<Expression> Value{};
+        
+        LetStatement() = default;
+        LetStatement(const Token& t) : TokenStatement(t) {};
+        LetStatement(const Token& t, std::shared_ptr<Identifier> i, std::shared_ptr<Expression> e) : TokenStatement(t), Name(i), Value(e) {};
+        ~LetStatement() = default;
 
         void statementNode() const override {};
         std::string TokenLiteral() const override { return TokenStatement.Literal; };
